f111aad789d74e807b6edcb339bd35b7ede986d7
Aneesh-oss <creadora08@gmail.com>
2024-03-22 12:17:27 +0530

    upload 3

---
+++ .github/index.py
@@ -0,0 +1,17 @@
+print("Hello World !")
+
+print('Hello World Once More')
+
+# Arithmetic Operators  
+num1 = 5    # integer value
+num2 = 7.0  # float value
+sum = num1 + num2     # addition of int and float values
+diff = num1 - num2    # subtraction of int and float values
+prod = num1 * num2    # multiplication of int and float values
+quotient = num1 / num2 # division of int by float
+remainder = num1 % num2 # modulus/remainder of the division of int by float
+print(sum)
+print(diff)
+print(prod)
+print(quotient)
+print(remainder)
